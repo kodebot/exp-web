6d65cfac1b8a309b2f44acf89b3400b22a9b044f
Manikandan Venkatraman <mvenkatraman@jet2.com>
2019-07-09 08:19:34 +0100

    update: remove bharatradios as it is hosted from github

--- bharatradios/app/boot.ts
+++
@@ -1,8 +0,0 @@
-//import "../jspm_packages/github/twbs/bootstrap@3.3.6/css/bootstrap.css!";
-import "../assets/css/bootstrap-slate.css!";
-import "../jspm_packages/npm/font-awesome@4.5.0/css/font-awesome.css!";
-import "../assets/css/radio-theme.css!";
-import "bootstrap";
-import "./shell/shell.component";
-
-
--- bharatradios/typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="react/react.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="misc.d.ts" />
